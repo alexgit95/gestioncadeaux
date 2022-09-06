876e1d7a9452cd5c514024fadd00547ddeea0f11
alexgit95 <45203119+alexgit95@users.noreply.github.com>
2022-09-06 13:19:23 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+		function loadInitPers(){
+		
+		allPersons=[];
+		
+		
+		let pers = new Person(1,"Name 1",150,false);
+		allPersons.push(pers);
+		
+		pers = new Person(3,"Name 2",100,false);
+		allPersons.push(pers);
+
+		pers = new Person(2,"Name 3",100,false);
+		allPersons.push(pers);
+
+		//to continue
+		
+		}
