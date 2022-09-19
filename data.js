f12990649198f1c3168902f751dc138970ac2ac3
alexgit95 <45203119+alexgit95@users.noreply.github.com>
2022-09-19 19:27:21 +0200

    Fixed issue with data comma

--- data.js
+++ data.js
@@ -6,10 +6,10 @@
 		let pers = new Person("Name 1",150);
 		allPersons.push(pers);
 		
-		pers = new Person("Name 2",100,);
+		pers = new Person("Name 2",100);
 		allPersons.push(pers);
 
-		pers = new Person("Name 3",100,);
+		pers = new Person("Name 3",100);
 		allPersons.push(pers);
 
 		//to continue
